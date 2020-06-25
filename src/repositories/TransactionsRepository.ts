4717a29ed67e7610364a9c5ea7074b4965dc840c
Gentil Douglas <geniuses.natus.vincere@gmail.com>
2020-06-25 15:19:22 -0300

    Desafio 05: Primeiro projeto Node.js

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,45 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    let income = 0;
+
+    this.transactions
+      .filter(item => item.type === 'income')
+      .map(item => {
+        income += item.value;
+        return income;
+      });
+
+    let outcome = 0;
+
+    this.transactions
+      .filter(item => item.type === 'outcome')
+      .map(item => {
+        outcome += item.value;
+        return outcome;
+      });
+
+    return {
+      income,
+      outcome,
+      total: income - outcome,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({
+      title,
+      value,
+      type,
+    });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
